97c3c0c8b0adb3e5e7015b4d0dfa686995b900b9
Joe Fredette <jfredett@gmail.com>
2017-12-22 00:05:24 +0000

    add guard for cargo package updates

--- watchman.sh
+++ watchman.sh
@@ -1,2 +1,3 @@
 watchman-make -p 'tests/*' --make 'clear; cargo' -t test \
-              -p 'src/*.rs' 'src/**/*.rs' --make 'clear; cargo' -t test
+              -p 'src/*.rs' 'src/**/*.rs' --make 'clear; cargo' -t test \
+              -p 'Cargo.toml' --make 'clear; cargo' -t update
